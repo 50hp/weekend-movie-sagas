ad9afd431b041d86075e93ff40ee96801514e7d2
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-06-16 09:07:46 -0500

    created the basic file structure for the details feature

---
+++ src/components/MovieList/MovieItem/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,16 @@
+
+
+
+function MovieDetails(){
+
+    return(
+
+
+
+
+    );
+
+
+
+}
+export default MovieDetails;
---
+++ src/components/MovieList/MovieItem/MovieItem.jsx
@@ -0,0 +1,25 @@
+
+import MovieDetails from "./MovieDetails/MovieDetails";
+
+
+
+
+
+function MovieItem({movie}) {
+
+
+
+    return (
+
+
+            <MovieDetails/>
+
+
+    );
+
+
+
+}
+
+
+export default MovieItem;
