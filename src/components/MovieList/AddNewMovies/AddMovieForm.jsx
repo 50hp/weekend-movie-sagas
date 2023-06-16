fb890375f2d32c798b3317fae2390fb5b007136f
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-06-16 12:20:11 -0500

    Created a more robust form with more inputs

--- src/components/MovieList/AddNewMovies/AddMovieForm.jsx
+++ src/components/MovieList/AddNewMovies/AddMovieForm.jsx
@@ -8,6 +8,11 @@ function AddNewMovie() {
     const results = useSelector(store => store.movieSearch);
     const [input, setInput] = useState(String);
 
+    const [inputOne, setInputOne] = useState(String);
+    const [inputTwo, setInputTwo] = useState(String);
+    const [inputThree, setInputThree] = useState(String);
+    const [inputFour, setInputFour] = useState(String);
+
     const handleSubmit = (event) => {
         event.preventDefault();
         dispatch({type:"SEARCH_MOVIES", payload: input});
@@ -26,6 +31,31 @@ function AddNewMovie() {
                        onChange={(e) => handleChange(e.target.value)} />
                 <button type="submit">Search</button> 
             </form>
+
+             <form onSubmit={(event)=> handleSubmit(event)}>
+                   <input type='text'
+                       placeholder="Title"
+                       value={inputOne}
+                       required
+                       onChange={(e) => handleChange(e.target.value)} />
+                    <input type='text'
+                       placeholder="Poster url"
+                       value={inputTwo}
+                       required
+                       onChange={(e) => handleChange(e.target.value)} />
+                    <input type='text'
+                       placeholder="Description"
+                       value={inputThree}
+                       required
+                       onChange={(e) => handleChange(e.target.value)} />
+                    <input type='text'
+                       value={inputFour}
+                       name="Genera"
+                       required
+                       onChange={(e) => handleChange(e.target.value)} />
+                <button type="submit">Search</button> 
+            </form>
+
             <div>
                  {results.map((movie, i) => (
                      <DisplayMovie key={i} movie={movie} />
